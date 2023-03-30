f6ca1b4c016eeed9681e77a49fc1d9c61608c6df
qdrx <qdrx@users.noreply.github.com>
2023-03-30 21:39:25 +0300

    add db to socket manager add films event

--- backend/src/modules/socket-manager.ts
+++ backend/src/modules/socket-manager.ts
@@ -1,22 +1,29 @@
 import * as http from "http";
 import {Socket} from "socket.io";
 import {RoomManager} from "./room-manager";
+import {Database} from "./database";
 
 export class SocketManager {
     public readonly io;
     private readonly httpServer: http.Server;
     private roomManager: RoomManager;
+    private database: Database;
 
-    constructor() {
+    constructor(database: Database) {
+        this.database = database;
         this.httpServer = require("http").createServer();
         this.roomManager = new RoomManager();
         this.io = require("socket.io")(this.httpServer);
         this.io.on("connection", (socket: Socket) => {
             console.log("A user connected: ", socket.id);
-            socket.on("createRoom", (filmPreferences: string[]) => {
+            socket.on("createRoom", async (filmPreferences: string[]) => {
                 const roomId = this.roomManager.generateRoomId()
                 const room = this.roomManager.createRoom(roomId);
-
+                const films = await this.database.getRandomFilmsByGenres(filmPreferences, 10);
+                console.log("Films: ", films)
+                for(let film of films) {
+                    room.films.push(film);
+                }
                 socket.emit("roomCreated", roomId);
             });
             socket.on("joinRoom", (roomId: string) => {
@@ -25,6 +32,7 @@ export class SocketManager {
                     room.users.push(socket.id);
                     socket.join(roomId);
                     socket.emit("roomJoined", roomId);
+                    socket.emit("films", room.films);
                 }
             });
             socket.on("disconnect", () => {
