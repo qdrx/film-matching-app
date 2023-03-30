9d34c12b871d05fd223a9165ee339715758c65ab
qdrx <qdrx@users.noreply.github.com>
2023-03-30 21:38:59 +0300

    add db to socket manager

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,14 @@ import {Database} from "./modules/database";
 import dotenv from "dotenv";
 
 dotenv.config();
-const socketManager: SocketManager = new SocketManager();
+
 if (!process.env.MONGO_URL || !process.env.MONGO_DB_NAME) {
     throw new Error("Missing environment variables");
 }
 const db = new Database(process.env.MONGO_URL?.toString(), process.env.MONGO_DB_NAME?.toString());
+const socketManager: SocketManager = new SocketManager(db);
+
+// for (let i = 0; i < 30; i++) {
+//     db.addRandomFilm().then(r => console.log("random film added"));
+// }
 // TODO: Finish create models for mongoose
