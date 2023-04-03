2df3dce5a0e31f297a61467f19dbf03f59550d42
qdrx <qdrx@users.noreply.github.com>
2023-04-03 04:18:32 +0300

    add findMatchedFilms function

--- backend/src/models/room.ts
+++ backend/src/models/room.ts
@@ -1,17 +1,22 @@
-import { Film } from "./film";
+import {Film} from "./film";
+import {IFilm} from "../interfaces/film";
 
 export class Room {
     id: string;
     films: Film[];
     users: string[];
-    bannedFilms: Set<string>;
-    acceptedFilms: Set<string>;
+    bannedFilms: Array<IFilm>;
+    acceptedFilms: Array<IFilm>;
 
     constructor(id: string) {
         this.id = id;
         this.films = [];
         this.users = [];
-        this.bannedFilms = new Set();
-        this.acceptedFilms = new Set();
+        this.bannedFilms = new Array<IFilm>();
+        this.acceptedFilms = new Array<IFilm>();
+    }
+
+    findMatchedFilms(arr: Array<IFilm>) {
+        return arr.filter((film, index) => arr.indexOf(film) != index);
     }
 }
